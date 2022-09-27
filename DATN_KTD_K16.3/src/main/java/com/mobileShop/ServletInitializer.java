7c180555b3f555f08ed2db9c2983a29bb1d54000
LeKhanhDuy0601 <duylkpc01980@fpt.edu.vn>
2022-09-27 19:23:05 +0700

    updata

--- DATN_KTD_K16.3/src/main/java/com/mobileShop/ServletInitializer.java
+++ DATN_KTD_K16.3/src/main/java/com/mobileShop/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.poly;
+package com.mobileShop;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
