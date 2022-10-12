df1b993984e5e0028393d9893bf2c3fc0c945207
LeKhanhDuy0601 <duylkpc01980@fpt.edu.vn>
2022-10-12 20:11:51 +0700

    up

--- MobileShop/src/main/java/MobileShop/Entity/CategoryGroup.java
+++ MobileShop/src/main/java/MobileShop/Entity/CategoryGroup.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,7 +31,7 @@ String	icon;
 Boolean	activity;
 Boolean	deleted;
 Integer	deleted_by;
-
+@JsonIgnore
 @OneToMany(mappedBy = "categoryGroup")
 List<Category> categories;
 
